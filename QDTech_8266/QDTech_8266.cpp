5526c4d00ea61d09d0b577807603c9936fed2aa3
norm8332 <norm@universal-cs.com>
2015-10-09 21:33:31 -0400

    Update QDTech_8266.cpp

--- QDTech_8266/QDTech_8266.cpp
+++ QDTech_8266/QDTech_8266.cpp
@@ -1,4 +1,25 @@
+
 /***************************************************
+  This is a modification of the Adafruit SPI TFT LCD Arduino 
+  library, customised for the ESP8266 and hardware SPI for 
+  the QDTech 1.8" 128x160 pixel LCD board using a Samsung 
+  S6D02A1 chip.
+  
+  The initialisation sequence comes from Henning Karlsen's
+  UTFT library: http://henningkarlsen.com
+
+  Using the hardware SPI pins is highly recommeneded.
+  
+  Unzip and change the folder name to "QDTech".
+  Copy the folder into your
+  <arduinosketchfolder>/libraries/ folder.
+  
+  You will also need the newest stock "Adafruit_GFX" library.
+  https://github.com/adafruit/Adafruit-GFX-Library
+
+  Includes code from Gilchrist 30/1/2014
+  Norm8332 - 10/9/2015
+***************************************************
   This is a library for the Adafruit 1.8" SPI display.
 
 This library works with the Adafruit 1.8" TFT Breakout w/SD card
